244ae515f273e20abeb3ddb2f81106183d87eb90
Rafael <rafaelongo45@gmail.com>
2022-06-07 22:51:32 -0300

    user ranking by visits count created. Fix: When displaying a user data it shows the correct amount on visitCount

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,29 +7,53 @@ export async function getUserData(req,res){
 
   try {
     const userRequest = await connection.query(`
-      SELECT users.id, users.name, SUM(links.visits) as "visitCount"
-      FROM users
-      JOIN links
-      ON users.id = $1
-      GROUP BY users.id;
+    SELECT users.id, users.name, SUM(COALESCE(links.visits, 0)) as "visitCount"
+    FROM users
+    LEFT JOIN links
+    ON users.id = links."creatorId"
+    WHERE users.id = $1
+    GROUP BY users.id;
     `, [id]);
     const user = userRequest.rows[0];
+    console.log(user)
 
-    const userLinksRequests = await connection.query(`
+    const userLinksRequest = await connection.query(`
       SELECT links.id, links."shortenedUrl" as "shortUrl", links."originalUrl" as url, links.visits as "visitCount"
       FROM links
       WHERE links."creatorId" = $1;
     `, [id]);
-    const userLinks = userLinksRequests.rows;
+    const userLinks = userLinksRequest.rows;
 
     const userStructure = {
       "id": user.id,
       "name": user.name,
       "visitCount": user.visitCount,
       "shortenedUrls": userLinks
-    }
+    };
 
     return res.status(200).send(userStructure);
+  } catch (e) {
+    console.log(chalk.bold.red('Erro no servidor'), e);
+    return res.sendStatus(500);
+  }
+}
+
+export async function getRanking(req,res){
+
+  try {
+    const rankingRequest = await connection.query(`
+      SELECT users.id, users.name, COUNT(links.id) AS "linksCount", 
+      SUM(coalesce(links.visits, 0)) AS "visitCount"
+      FROM users
+      LEFT JOIN links
+      ON users.id = links."creatorId"
+      GROUP BY users.id
+      ORDER BY ("visitCount") DESC
+      LIMIT 10;
+    `);
+    const ranking = rankingRequest.rows;
+
+    return res.status(200).send(ranking); 
   } catch (e) {
     console.log(chalk.bold.red('Erro no servidor'), e);
     return res.sendStatus(500);
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
 import { userExists } from "../middlewares/validateUser.js";
-import { getUserData } from "../controllers/usersController.js";
+import { getRanking, getUserData } from "../controllers/usersController.js";
 
 const userRouter = Router();
 
+userRouter.get('/users/ranking', getRanking);
 userRouter.get('/users/:id', userExists, getUserData);
 
 export default userRouter;
