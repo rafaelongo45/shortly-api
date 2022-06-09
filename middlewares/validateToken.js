edd42b7d92b56642485445d5d2cf518867a90b88
Rafael <rafaelongo45@gmail.com>
2022-06-09 03:03:56 -0300

    Alteration in route name. When getting user stats you now have to send a valid token that needs to be linked to the correct user id

--- middlewares/validateToken.js
+++ middlewares/validateToken.js
@@ -30,4 +30,42 @@ export async function validateToken(req,res,next){
     console.log(chalk.bold.red('Erro no servidor'), e);
     return res.sendStatus(500);
   }
-}
+};
+
+export async function validateTokenUserRoute(req,res,next){
+  const { authorization } = req.headers;
+  const token = authorization?.replace('Bearer', '').trim();
+  const params = [token]
+  let idQuery = '';
+
+  if(!token){
+    return res.sendStatus(401);
+  }
+
+  if(req.params.id){
+    idQuery = `AND "userId" = $2`
+    params.push(req.params.id)
+  }
+
+  try {
+    const request = await connection.query(`
+      SELECT * 
+      FROM sessions
+      WHERE token = $1
+      ${idQuery}
+    `, params);
+
+    const userSession = request.rows[0];
+
+    if(!userSession){
+      return res.sendStatus(401);
+    }
+    
+    res.locals.userId = userSession.userId;
+    
+    next();
+  } catch (e) {
+    console.log(chalk.bold.red('Erro no servidor'), e);
+    return res.sendStatus(500);
+  }
+};
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
 import { userExists } from "../middlewares/validateUser.js";
+import { validateTokenUserRoute } from "../middlewares/validateToken.js";
 import { getRanking, getUserData } from "../controllers/usersController.js";
 
 const userRouter = Router();
 
-userRouter.get('/users/ranking', getRanking);
-userRouter.get('/users/:id', userExists, getUserData);
+userRouter.get('/ranking', getRanking);
+userRouter.get('/users/:id', userExists, validateTokenUserRoute, getUserData);
 
 export default userRouter;
